4b0c5a6229407258eb381c04f3c1571cb2fe0de6
Zarathustra001 <arthurgarankin@gmail.com>
2024-06-10 22:42:30 +0300

    пересобрал, добавил комментарии

--- Дополнительное практическое задание по модулю/Средний балл.py
+++ Дополнительное практическое задание по модулю/Средний балл.py
@@ -1,11 +1,28 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
+# Преобразуем множество students в упорядоченный список
+students_list = sorted(students)
+
+# Создаем пустой словарь для хранения средних баллов
 average_grades = {}
 
-for i, student in enumerate(students):
-    total_grade = sum(grades[i])
-    average_grade = total_grade / len(grades[i])
-    average_grades[student] = average_grade
+# Перебираем в цикле имена учеников
+for student in students_list:
+    # Получаем список оценок для ученика по его имени
+    student_grades = grades[students_list.index(student)]
 
+    # Считаем сумму оценок ученика
+    total_grade = sum(student_grades)
+
+    # Считаем количество оценок ученика
+    num_grades = len(student_grades)
+
+    # Вычисляем средний балл ученика
+    average = total_grade / num_grades
+
+    # Добавляем имя ученика и его средний балл в словарь
+    average_grades[student] = average
+
+# Выводим результат в консоль
 print(average_grades)
