dc43b913d3ba61a388de7488590dae45f02aba92
Zarathustra001 <arthurgarankin@gmail.com>
2024-06-19 01:19:36 +0300

    Update Дополнительное практическое задание по модулю Основные операторы2.py

--- Дополнительное практическое задание по модулю/Дополнительное практическое задание по модулю Основные операторы2.py
+++ Дополнительное практическое задание по модулю/Дополнительное практическое задание по модулю Основные операторы2.py
@@ -1,10 +1,10 @@
 # Ввод числа от пользователя от 3 до 20
 number = int(input("Введите число от 3 до 20: "))
-while number < 3 or число > 20:
+while number < 3 or number > 20:
     number = int(input("Введите число от 3 до 20: "))
 
 # Вторая вставка для пар чисел
-Pass_for_number = ""
+pass_for_number = ""
 
 # Перебираем числа от 1 до 20
 for i in range(1, 20):
