d2103f09b8c79dafbaecab6a200bec8bcf9fc9ca
Zarathustra001 <arthurgarankin@gmail.com>
2024-06-30 11:57:59 +0300

    module_4_1

---
+++ module_4_1/module_4_1.py
@@ -0,0 +1,17 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+while True:
+    try:
+        first = float(input("Введите первое число: "))
+        second = float(input("Введите второе число: "))
+        break
+    except ValueError:
+        print("Пожалуйста, введите корректные числовые значения.")
+
+# Вызов функций
+result_fake = fake_divide(first, second)
+result_true = true_divide(first, second)
+
+print(f"Результат деления {first} на {second} в fake_math: {result_fake}")
+print(f"Результат деления {first} на {second} в true_math: {result_true}")
