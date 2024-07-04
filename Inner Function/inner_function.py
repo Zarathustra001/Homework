ec9863c105c1d96349ff504cb4df9106e7dbf2aa
Zarathustra001 <arthurgarankin@gmail.com>
2024-07-04 22:10:12 +0300

    Create inner_function.py

---
+++ Inner Function/inner_function.py
@@ -0,0 +1,14 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()  # Вызов inner_function внутри test_function
+
+
+test_function()  # Вызов test_function (и, следовательно, inner_function)
+
+# Попытка вызвать inner_function вне test_function
+try:
+    inner_function()
+except NameError as a:
+    print(f"Ошибка: {a}")
