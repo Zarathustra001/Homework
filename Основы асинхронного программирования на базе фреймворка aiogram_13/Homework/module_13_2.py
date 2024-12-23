5185e543d6d95c6f7c98bd2332e082104602f7c1
Zarathustra001 <arthurgarankin@gmail.com>
2024-12-23 13:47:49 +0300

    Create module_13_2.py

---
+++ Основы асинхронного программирования на базе фреймворка aiogram_13/Homework/module_13_2.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ""    #Ключ
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+# Функция для обработки команды /start
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.reply('Привет! Я бот помогающий твоему здоровью.')
+
+# Функция для обработки всех остальных сообщений
+@dp.message_handler()
+async def all_messages(message: types.Message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.reply('Введите команду /start, чтобы начать общение.')
+
+if __name__ == "__main__":
+    print("Запуск бота...")
+    executor.start_polling(dp, skip_updates=True)
