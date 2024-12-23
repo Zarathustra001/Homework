191477fe6539e311c590f64e9a0e12e0ae765507
Zarathustra001 <arthurgarankin@gmail.com>
2024-12-23 15:09:08 +0300

    Create module_13_4.py

---
+++ Основы асинхронного программирования на базе фреймворка aiogram_13/Homework/module_13_4.py
@@ -0,0 +1,54 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+
+API_TOKEN = "" #ключ
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+@dp.message_handler(commands=['start'])
+async def start_message(message: types.Message):
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+@dp.message_handler(text="Calories")
+async def set_age(message: types.Message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message: types.Message, state: FSMContext):
+    await state.update_data(age=message.text)  # Сохраняем возраст
+    await message.answer("Введите свой рост (в см):")
+    await UserState.growth.set()
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message: types.Message, state: FSMContext):
+    await state.update_data(growth=message.text)  # Сохраняем рост
+    await message.answer("Введите свой вес (в кг):")
+    await UserState.weight.set()
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message: types.Message, state: FSMContext):
+    await state.update_data(weight=message.text)  # Сохраняем вес
+    data = await state.get_data()  # Получаем все данные
+
+    # Извлекаем данные
+    age = int(data['age'])
+    growth = int(data['growth'])
+    weight = int(data['weight'])
+
+    # Формула Миффлина - Сан Жеора для мужчин
+    calories = 10 * weight + 6.25 * growth - 5 * age + 5
+
+    await message.answer(f"Ваша норма калорий: {calories:.2f} ккал.")
+    await state.finish()  # Завершаем машину состояний
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
