2a526ec268b58421c8febb9b956240c1df203fe0
Zarathustra001 <arthurgarankin@gmail.com>
2025-01-24 14:57:06 +0300

    Create module_16_3.py

---
+++ Основы Fast Api_16/homework/module_16_3.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = {'1': 'Имя: Example, возраст: 18'}
+
+
+@app.get("/users")
+def get_users():
+    return users
+
+
+@app.post("/user/{username}/{age}")
+def create_user(username: str, age: int):
+    user_id = str(max([int(k) for k in users.keys()]) + 1)
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} is registered"
+
+
+@app.put("/user/{user_id}/{username}/{age}")
+def update_user(user_id: str, username: str, age: int):
+    if user_id in users:
+        users[user_id] = f"Имя: {username}, возраст: {age}"
+        return f"User {user_id} has been updated"
+    else:
+        return "User not found"
+
+
+@app.delete("/user/{user_id}")
+def delete_user(user_id: str):
+    if user_id in users:
+        del users[user_id]
+        return f"User {user_id} has been deleted"
+    else:
+        return "User not found"
