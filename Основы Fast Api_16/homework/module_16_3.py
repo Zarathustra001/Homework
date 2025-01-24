499cc2f23f4eafb743351579fd0566de66f093d8
Zarathustra001 <arthurgarankin@gmail.com>
2025-01-24 15:00:52 +0300

    Update module_16_3.py

--- Основы Fast Api_16/homework/module_16_3.py
+++ Основы Fast Api_16/homework/module_16_3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
@@ -12,6 +12,12 @@ def get_users():
 
 @app.post("/user/{username}/{age}")
 def create_user(username: str, age: int):
+    if not username:
+        raise HTTPException(status_code=400, detail="Username cannot be empty")
+
+    if age <= 0:
+        raise HTTPException(status_code=400, detail="Age must be a positive number")
+
     user_id = str(max([int(k) for k in users.keys()]) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
@@ -19,17 +25,23 @@ def create_user(username: str, age: int):
 
 @app.put("/user/{user_id}/{username}/{age}")
 def update_user(user_id: str, username: str, age: int):
-    if user_id in users:
-        users[user_id] = f"Имя: {username}, возраст: {age}"
-        return f"User {user_id} has been updated"
-    else:
-        return "User not found"
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if not username:
+        raise HTTPException(status_code=400, detail="Username cannot be empty")
+
+    if age <= 0:
+        raise HTTPException(status_code=400, detail="Age must be a positive number")
+
+    users[user_id] = f"Имя: {username}, возраст: {age}"
+    return f"User {user_id} has been updated"
 
 
 @app.delete("/user/{user_id}")
 def delete_user(user_id: str):
-    if user_id in users:
-        del users[user_id]
-        return f"User {user_id} has been deleted"
-    else:
-        return "User not found"
+    if user_id not in users:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    del users[user_id]
+    return f"User {user_id} has been deleted"
