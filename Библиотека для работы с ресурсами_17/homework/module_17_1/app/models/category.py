58b4e3bae19d48d83f58731d7d7aedf13485e88b
Zarathustra001 <arthurgarankin@gmail.com>
2025-01-29 12:20:15 +0300

    Update category.py

--- Библиотека для работы с ресурсами_17/homework/module_17_1/app/models/category.py
+++ Библиотека для работы с ресурсами_17/homework/module_17_1/app/models/category.py
@@ -4,8 +4,15 @@ from sqlAlchemy.orm import relationship
 
 class Category(Base):
   __tablename__="categories"
+  __table_args__ = {'keep_existing': True}
   id = Column(Integer, primary_key=True, index=True)
   name = Column(String)
   slug= Column(String, unique=True, index=True)
   is_active = Column(Boolean, default=True)
+  parent_id = Column(Integer, ForeignKey("categories.id"), nullable= True)
+
+  products = relationship("Product", back_populates="category")
+
+from sqlalchemy.shcema import CreateTable
+print(CreateTable(Category.__table__))
   
