560c2fb9235df36790c9b480b0ce0221a72df032
Zarathustra001 <arthurgarankin@gmail.com>
2025-01-29 10:53:54 +0300

    bo

---
+++ Библиотека для работы с ресурсами_17/homework/module_17_1/app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from task import router as task_router
+from user import router as user_router
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to Taskmanager"}
+
+# Подключение маршрутов
+app.include_router(task_router)
+app.include_router(user_router)
