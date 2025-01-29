b1801e48f1ed1ae898788bbab3915fc5a9662a88
Zarathustra001 <arthurgarankin@gmail.com>
2025-01-29 11:53:55 +0300

    Update category

--- Библиотека для работы с ресурсами_17/homework/module_17_1/app/models/category.py
+++ Библиотека для работы с ресурсами_17/homework/module_17_1/app/models/category.py
@@ -1,1 +1,11 @@
+fromm app.backend.db import Base
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+from sqlAlchemy.orm import relationship
 
+class Category(Base):
+  __tablename__="categories"
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String)
+  slug= Column(String, unique=True, index=True)
+  is_active = Column(Boolean, default=True)
+  
