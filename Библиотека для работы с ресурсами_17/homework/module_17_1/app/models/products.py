05dfcb43b896c031bc086a21c8e1eb3740ff63d8
Zarathustra001 <arthurgarankin@gmail.com>
2025-01-29 12:20:20 +0300

    Update products.py

--- Библиотека для работы с ресурсами_17/homework/module_17_1/app/models/products.py
+++ Библиотека для работы с ресурсами_17/homework/module_17_1/app/models/products.py
@@ -4,6 +4,7 @@ from sqlAlchemy.orm import relationship
 
 class Product(Base):
   __tablename__="products"
+  __table_args__ = {'keep_existing': True}
   id = Column(Integer, primary_key=True, index=True)
   name = Column(String)
   slug= Column(String, unique=True, index=True)
@@ -11,6 +12,9 @@ class Product(Base):
   price = Column(Integer)
   image_url = Column(String)
   stock = Column(Integer)
+  category_id = Column(Integer, ForeignKey('categories.id'))
   rating = Column(Float)
   is_active = Column(Boolean, default=True)
+
+category = relationship('Category', back_populates='products')
   
